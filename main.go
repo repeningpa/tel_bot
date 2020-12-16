efe6adb9cf8e43461ce949d2fdfc8dad007dc701
repeningpa <repening.pavel@gmail.com>
2020-12-16 20:04:17 +0400

    create: tel_bot

--- main.go
+++ main.go
@@ -17,29 +17,34 @@ func main() {
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
 	// инициализируем канал, куда будут прилетать обновления от API
-	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
-	ucfg.Timeout = 60
-	err = bot.UpdatesChan(ucfg)
-	// читаем обновления из канала
-	for {
-		select {
-		case update := <-bot.Updates:
-			// Пользователь, который написал боту
-			UserName := update.Message.From.UserName
-			// ID чата/диалога.
-			// Может быть идентификатором как чата с пользователем
-			// (тогда он равен UserID) так и публичного чата/канала
-			ChatID := update.Message.Chat.ID
-			// Текст сообщения
-			Text := update.Message.Text
-			log.Printf("[%s] %d %s", UserName, ChatID, Text)
-			// Ответим пользователю его же сообщением
-			reply := Text
-			// Созадаем сообщение
-			msg := tgbotapi.NewMessage(ChatID, reply)
-			// и отправляем его
-			bot.SendMessage(msg)
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates, err := bot.GetUpdatesChan(u)
+
+	// в канал updates прилетают структуры типа Update
+	// вычитываем их и обрабатываем
+	for update := range updates {
+		// универсальный ответ на любое сообщение
+		reply := "Не знаю что сказать"
+		if update.Message == nil {
+			continue
 		}
 
+		// логируем от кого какое сообщение пришло
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		// свитч на обработку комманд
+		// комманда - сообщение, начинающееся с "/"
+		switch update.Message.Command() {
+		case "start":
+			reply = "Привет. Я телеграм-бот"
+		case "hello":
+			reply = "world"
+		}
+
+		// создаем ответное сообщение
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		// отправляем
+		bot.Send(msg)
 	}
 }
