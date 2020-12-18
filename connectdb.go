906ae687cff4c06a0a2233993f6311d79ee82081
repeningpa <repening.pavel@gmail.com>
2020-12-18 16:16:15 +0400

    add: connect DB

---
+++ connectdb.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+)
+
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = ""
+	dbname   = "tg_bot"
+)
+
+func connectdb() (database *sql.DB) {
+
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlInfo)
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	database = db
+	defer db.Close()
+
+	return database
+
+}
--- main.go
+++ main.go
@@ -9,6 +9,8 @@ import (
 	"log"
 	"net/http"
 
+	_ "github.com/lib/pq"
+
 	tgbotapi "github.com/Syfaro/telegram-bot-api"
 )
 
@@ -17,28 +19,13 @@ type Person struct {
 	chatID int
 }
 
-//Token ...
-type Token struct {
-	token int
-}
+var token string
 
 var database *sql.DB
 
 func main() {
 
-	db, err := sql.Open("postgres", "postres: @/tg_bot")
-
-	if err != nil {
-		log.Println(err)
-	}
-
-	database = db
-	defer db.Close()
-
-	person := GetPerson()
-	for _, pe := range person {
-		fmt.Println(pe.chatID)
-	}
+	database = connectdb()
 
 	bot, err := tgbotapi.NewBotAPI("1409094275:AAFBP7Vm2D-soxzHIht9pYXACDLOiJDqLdM")
 	if err != nil {
@@ -48,21 +35,89 @@ func main() {
 	bot.Debug = true
 	log.Printf("Authorized on account %s", bot.Self.UserName)
 
-	////////////////////
-	message := map[string]interface{}{
-		"chat_id": "496818745",
-		"text":    "Меня пересобрали...",
+	SendMessage(bot)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 30
+	updates, err := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		reply := "Команда не найдена..."
+
+		if update.Message.From.UserName != "Ell" {
+
+			switch update.Message.Command() {
+			case "hello":
+				reply = "world"
+			case "fendi":
+				reply = "FENDI GUCHI FLIP-FLOP"
+			}
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+			bot.Send(msg)
+		}
+	}
+}
+
+//GetPerson ...
+func GetPerson(database *sql.DB) (per []*Person) {
+
+	rows, err := database.Query("select * from person")
+	if err != nil {
+		log.Println(err)
+	}
+	defer rows.Close()
+
+	per = make([]*Person, 0)
+	for rows.Next() {
+		pe := new(Person)
+		err := rows.Scan(&pe.chatID)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		per = append(per, pe)
 	}
 
-	bytesRepresentation, err := json.Marshal(message)
+	return per
+}
+
+//GetToken ...
+func GetToken(database *sql.DB) (token string) {
+	err := database.QueryRow("select token from tg_main").Scan(&token)
 	if err != nil {
-		log.Fatalln(err)
+		log.Println(err)
 	}
 
-	resp, err := http.Post("https://api.telegram.org/bot1409094275:AAFBP7Vm2D-soxzHIht9pYXACDLOiJDqLdM/sendMessage", "application/json", bytes.NewBuffer(bytesRepresentation))
-	if err != nil {
-		log.Fatalln(err)
-		log.Fatalln(resp)
+	return token
+}
+
+//SendMessage ...
+func SendMessage(bot *tgbotapi.BotAPI) {
+	person := GetPerson(database)
+
+	arrChatID := make([]int, 0)
+	for _, pe := range person {
+		arrChatID = append(arrChatID, pe.chatID)
+
+		message := map[string]interface{}{
+			"chat_id": pe.chatID,
+			"text":    "Меня пересобрали...",
+		}
+
+		bytesRepresentation, err := json.Marshal(message)
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		resp, err := http.Post("https://api.telegram.org/bot1409094275:AAFBP7Vm2D-soxzHIht9pYXACDLOiJDqLdM/sendMessage", "application/json", bytes.NewBuffer(bytesRepresentation))
+		if err != nil {
+			log.Fatalln(err)
+			log.Fatalln(resp)
+		}
 	}
 
 	res, err := http.Get("http://img10.joyreactor.cc/pics/post/full/%D0%9A%D0%9F%D0%91%D0%A1%D0%90-Anime-%D1%84%D1%8D%D0%BD%D0%B4%D0%BE%D0%BC%D1%8B-%D0%B0%D0%B3%D0%B8%D1%82%D0%B0%D1%86%D0%B8%D0%BE%D0%BD%D0%BD%D1%8B%D0%B5-%D0%BF%D0%BB%D0%B0%D0%BA%D0%B0%D1%82%D1%8B-5580394.jpeg")
@@ -76,82 +131,10 @@ func main() {
 	}
 
 	image := tgbotapi.FileBytes{Name: "build", Bytes: fileByte}
-	_, err = bot.Send(tgbotapi.NewPhotoUpload(int64(496818745), image))
-	if err != nil {
-		log.Fatalln(err)
-	}
-	////////////////////
-
-	u := tgbotapi.NewUpdate(0)
-	u.Timeout = 30
-	updates, err := bot.GetUpdatesChan(u)
-
-	var reply = ""
-	for update := range updates {
-
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-		switch update.Message.Command() {
-		case "привет":
-			reply = "Пароль не верный."
-		case "hello":
-			reply = "world"
-		case "fendi":
-			reply = "FENDI GUCHI FLIP-FLOP"
-		}
-
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
-		bot.Send(msg)
-	}
-}
-
-//GetPerson ...
-func GetPerson() (per []*Person) {
-
-	rows, err := database.Query("select * from rg_bot.person")
-	if err != nil {
-		log.Println(err)
-	}
-	defer rows.Close()
-	// person = []Person{}
-
-	per = make([]*Person, 0)
-	for rows.Next() {
-		pe := new(Person)
-		err := rows.Scan(&pe.chatID)
+	for _, arr := range arrChatID {
+		_, err = bot.Send(tgbotapi.NewPhotoUpload(int64(arr), image))
 		if err != nil {
-			fmt.Println(err)
-			return
+			log.Fatalln(err)
 		}
-		per = append(per, pe)
 	}
-
-	// for _, pe := range per {
-	// 	fmt.Println(pe.chatID)
-	// }
-
-	return per
 }
-
-// //GetToken ...
-// func GetToken() {
-
-// 	rows, err := database.Query("select * from rg_bot.tg_main")
-// 	if err != nil {
-// 		log.Println(err)
-// 	}
-// 	defer rows.Close()
-// 	token := []Token{}
-
-// 	for rows.Next() {
-// 		p := Token{}
-// 		err := rows.Scan(&p.token)
-// 		if err != nil {
-// 			fmt.Println(err)
-// 			continue
-// 		}
-// 		token = append(token, p)
-// 	}
-
-// 	return token
-// }
